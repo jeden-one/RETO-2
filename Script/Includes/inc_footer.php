3cd58de8109d81fa7d8f8f55cc8ee27dc86f817f
miguelbarrosv <miguel.barros@ikasle.egibide.org>
2019-11-07 14:01:34 +0100

    inicio de includes

---
+++ Script/Includes/inc_footer.php
@@ -0,0 +1,20 @@
+<?php ?>
+
+<img src="../../img/flecha.svg" id="flecha">
+<head>
+    <link rel="stylesheet" href="../../CSS/footer.css">
+</head>
+<footer>
+    <p>Siguenos en nuestras redes sociales</p>
+
+    <div id="divIconos">
+        <img src="../../img/facebook.svg">
+        <img src="../../img/twitter.svg">
+        <img src="../../img/instagram.svg">
+    </div>
+
+    <p>Copyright © Todos los Derechos Reservados </p>
+</footer>
+</body>
+
+
---
+++ Script/Includes/inc_header.php
@@ -0,0 +1,10 @@
+<?php
+ ?>
+<head>
+    <link rel="stylesheet" href="../../CSS/header.css">
+</head>
+<body>
+    <header>
+        <a href="../index.php"><img src="../../img/ajebask.jpeg"></a>
+    </header>
+
