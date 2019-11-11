f6ed4d5baeda5785c3b11550f5fc85781fe161ec
miguelbarrosv <miguel.barros@ikasle.egibide.org>
2019-11-11 10:46:02 +0100

    categorias.php y anuncio.php

---
+++ Script/anuncio.php
@@ -0,0 +1,33 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="../CSS/styles.css">
+</head>
+<body>
+<div id="contenedor4">
+    <header>
+        <img src="../img/ajebask.jpeg">
+        <p>Mas de "numero" de anuncios publicados en nuestra pagina web</p>
+    </header>
+    <div id="containerAnuncio">
+        <div id="navAnuncio">
+            <img src="../img/ajebask.jpeg">
+            <p>Nombre del anunciante</p>
+            <input type="button" value="Contactar">
+        </div>
+        <img src="../img/coche.jpeg" id="imgAnuncio">
+        <h2>Titulo del producto</h2>
+        <label>1500$</label>
+        <p>sdrglafffffffffeiughslrfs ajgfsbkdfsfjlblds kdfvdfnhyjntdjhnyjtyyyy yyyyyyyyyyyyf</p>
+    </div>
+    <?php include("Includes/inc_anunciosCategoria.php")?>
+    <?php include("Includes/inc_footer.php")?>
+</div>
+</body>
+
+</html>
