12ac583f5bc52e5ec943433d35f7a6bcb0e8c1d4
Miguel <miguel.barros@ikasle.egibide.org>
2019-11-11 23:47:55 +0100

    update

--- Script/Includes/inc_header2.php
+++ Script/Includes/inc_header2.php
@@ -7,13 +7,11 @@
     <meta charset="UTF-8">
     <title>Title</title>
     <link href="../../CSS/loginRegistro.css" rel="stylesheet">
-    <script src="../index.js"></script>
     <script src="../../librerias/jQuery/jquery-3.4.1.js"></script>
 </head>
 
 <body>
 <div id="contenedor2">
-
     <header>
         <img src="../../img/ajebask.jpeg">
         <p>Mas de "numero" de anuncios publicados en nuestra pagina web</p>
