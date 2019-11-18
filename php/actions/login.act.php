3076c103acfdf9a166698051d3e1d42f3354edb9
Eric <eric.munoz@ikasle.egibide.org>
2019-11-18 12:28:15 +0100

    login perfeccionado

--- php/actions/login.act.php
+++ php/actions/login.act.php
@@ -2,16 +2,14 @@
 include "../database/mysql.php";
 $dbh = connect();
 
-if (isset($_POST["usuario"]) && isset($_POST["pass"])) {
+if (isset($_POST["usuario"]) && isset($_POST["pass"])&&$_POST['pass']!='') {
     $pass = $_POST["pass"];
     $usuario = $_POST["usuario"];
     $resultado = searchUsuarioOneEmail($dbh, $usuario);
     close($dbh);
-    if ($resultado == false) {
-        header("location: ../login.php?error=2");
-    } else {
+    if ($resultado == true) {
         if (password_verify($pass, $resultado->password)) {
-            setcookie("usuario",$_POST["usuario"],time()+(60*60*24*7),"/");
+            setcookie("usuario", $_POST["usuario"], time() + (60 * 60 * 24 * 7), "/");
             switch ($_POST['action']) {
                 case 'publicarAnuncio':
                     header("location: ../publicarAnuncio.php");
@@ -29,9 +27,7 @@ if (isset($_POST["usuario"]) && isset($_POST["pass"])) {
         } else {
             header("location: ../login.php?error=1");
         }
-
     }
-
 } else {
-    echo 'no datos';
+    header("location: ../login.php?error=2");
 }
