46d759710a98d8c7377624c5b1bdd969c3f2a855
miguelbarrosv <miguel.barros@ikasle.egibide.org>
2019-11-19 14:08:25 +0100

    update

--- php/editarPerfil.php
+++ php/editarPerfil.php
@@ -25,12 +25,12 @@ include "includes/editarPerfil.logic.php";
     <form id="datos" action="actions/editarPerfil.act.php" method="post" enctype="multipart/form-data">
         <?php mensajeRespuesta() ?>
         <input type="hidden" name="passwordPasar" value="<?php echo $password;?>">
-        <label>Foto: </label><input type="file" name="foto" accept="image/x-png,image/gif,image/jpeg">
-        <label>Nombre: </label><input type="text" name="nombre" value="<?php echo $nombre;?>">
-        <label>Contraseña: </label><input type="text" name="password" value="">
-        <label>Repite contraseña: </label><input type="text" name="repetirPassword" value="">
-        <label>Email: </label><input type="text" name="email" value="<?php echo $usuario;?>">
-        <label>Descripcion: </label><textarea name="descripcion" value="<?php echo $descripcion;?>"><?php echo $descripcion;?></textarea>
+        <label><p class="text">Foto:</p> </label><input type="file" name="foto" accept="image/x-png,image/gif,image/jpeg">
+        <label><p class="text">Nombre:</p> </label><input type="text" name="nombre" value="<?php echo $nombre;?>">
+        <label><p class="text">Contraseña:</p> </label><input type="text" name="password" value="">
+        <label><p class="text">Repite contraseña:</p> </label><input type="text" name="repetirPassword" value="">
+        <label><p class="text">Email:</p> </label><input type="text" name="email" value="<?php echo $usuario;?>">
+        <label><p class="text">Descripcion:</p> </label><textarea name="descripcion" value="<?php echo $descripcion;?>"><?php echo $descripcion;?></textarea>
 
         <input type="submit" value="Actualizar Usuario">
     </form>
